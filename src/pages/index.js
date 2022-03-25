4961972c74f4a19229e8cd578205894599101b90
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2022-03-25 11:10:45 +0100

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import "../css/pages/index.css";
+import portrait from "../images/portrait.webp";
+import heroshot from "../images/heroshot.webp";
 import { Helmet } from "react-helmet"
 
 
@@ -16,7 +18,33 @@ const IndexPage = () => (
     <body className="index" />
     </Helmet>
 
-    <h1>Building<br/> digital<br/> interfaces<br/> for KeyShot</h1>
+    <header>
+      <h1>Building<br/> digital<br/> interfaces<br/> for KeyShot</h1>
+      <div className="details">
+        <div className="details__image">
+          <img src={portrait} alt="" />
+          <div className="clearfix"></div>
+        </div>
+        <div className="details__info">
+          <h2>Steffen Muldbjerg</h2>
+          <p>Frontend developer  |  Luxion</p>
+        </div>
+      </div>
+    </header>
+
+    <nav className="mainNav">
+      <ul>
+        <li><a href="">Work</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+      </ul>
+    </nav>
+
+    <div className="heroShot">
+      <img src={heroshot} alt="" />
+    </div>
+
+    
     
     
   </Layout>
