514e01a5ce18e8758293dab3dc4ed4bfdca88d47
Steffen Muldbjerg <steffen.muldbjerg@luxion.com>
2022-07-12 21:06:35 +0200

    Reset blog + environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MarkdownPipe } from './core/pipes/markdown.pipe';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { HerospotComponent } from './components/index/herospot/herospot.component';
+import { TestPipe } from './core/pipes/test.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,7 @@ import { HerospotComponent } from './components/index/herospot/herospot.componen
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+  styleUrls: ['./play.component.scss'],
 })
 export class PlayComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
