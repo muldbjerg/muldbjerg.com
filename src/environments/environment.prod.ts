9410a3aed123ed540651ee723c7882c8b2345527
Steffen Muldbjerg <steffen.muldbjerg@luxion.com>
2022-07-14 22:46:44 +0200

    Restart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  contentful: JSON.parse(process.env.CONTENTFUL as string),
+  contentful: {
+    space: 'wnhx8rt8bx4x',
+    accessToken: 'VcwPYiKewYvDWnbOqLJS_48YVy_hH9yKq8odPN9nzCM',
+  },
 };
