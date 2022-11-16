f470cdf257ed257b92e1ba0bb2eb4e34105b1dbb
Steffen Muldbjerg <steffen.muldbjerg@luxion.com>
2022-11-16 21:54:45 +0100

    Fixed index rendering

--- src/app/components/index/herospot/herospot.component.ts
+++ src/app/components/index/herospot/herospot.component.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostListener,
   OnInit,
+  Renderer2,
   ViewChild,
 } from '@angular/core';
 
@@ -16,53 +17,71 @@ export class HerospotComponent {
     transform: 'translateX(0px) translateY(0px)',
   };
 
+  private unlistener: any[] = [];
   imgOpacity = {};
+  isSafari: boolean | string = false;
+  isTouchDevice = false;
 
-  isSafari =
-    navigator.vendor &&
-    navigator.vendor.indexOf('Apple') > -1 &&
-    navigator.userAgent &&
-    navigator.userAgent.indexOf('CriOS') == -1 &&
-    navigator.userAgent.indexOf('FxiOS') == -1;
+  constructor(private renderer2: Renderer2) {}
 
-  @HostListener('mousemove', ['$event']) onMouseOver(event: any) {
-    if (!this.isTouchDevice()) {
-      var customOffsetX = 50,
-        customOffsetY = 40,
-        x = (event.pageX - window.innerWidth) / 20 + customOffsetX,
-        y = (event.pageY - window.innerHeight) / 20 + customOffsetY;
+  ngOnInit(): void {
+    this.isTouchDevice =
+      'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-      this.imgTransform = {
-        transform: 'translateX(' + x + 'px) translateY(' + y + 'px)',
-      };
-    }
-  }
+    this.isSafari =
+      navigator.vendor &&
+      navigator.vendor.indexOf('Apple') > -1 &&
+      navigator.userAgent &&
+      navigator.userAgent.indexOf('CriOS') == -1 &&
+      navigator.userAgent.indexOf('FxiOS') == -1;
 
-  @HostListener('mouseleave', ['$event']) onMouseLeave(event: any) {
-    if (!this.isTouchDevice()) {
-      this.imgTransform = {
-        transform: 'translateX(0px) translateY(0px)',
-      };
-    }
-  }
-
-  @HostListener('window:scroll', ['$event']) onScroll(event: any) {
-    if (this.isTouchDevice()) {
-      this.imgOpacity = {
-        opacity: window.scrollY / 75,
-      };
-    }
-  }
-
-  constructor() {
-    if (this.isTouchDevice()) {
+    // Change graphic if touch device
+    if (this.isTouchDevice) {
       this.imgOpacity = {
         opacity: '0',
       };
     }
+
+    // Detect mouse movement when mouse over
+    this.unlistener.push(
+      this.renderer2.listen('document', 'mousemove', (event) => {
+        if (!this.isTouchDevice) {
+          var customOffsetX = 50,
+            customOffsetY = 40,
+            x = (event.pageX - window.innerWidth) / 20 + customOffsetX,
+            y = (event.pageY - window.innerHeight) / 20 + customOffsetY;
+
+          this.imgTransform = {
+            transform: 'translateX(' + x + 'px) translateY(' + y + 'px)',
+          };
+        }
+      })
+    );
+
+    // Detect when mouse leaves - reset animation
+    this.unlistener.push(
+      this.renderer2.listen('document', 'mouseleave', (event) => {
+        if (!this.isTouchDevice) {
+          this.imgTransform = {
+            transform: 'translateX(0px) translateY(0px)',
+          };
+        }
+      })
+    );
+
+    // Detect scroll for touch devices
+    this.unlistener.push(
+      this.renderer2.listen('window', 'onscroll', (event) => {
+        if (this.isTouchDevice) {
+          this.imgOpacity = {
+            opacity: window.scrollY / 75,
+          };
+        }
+      })
+    );
   }
 
-  isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  ngOnDestroy(): void {
+    this.unlistener = [];
   }
 }
