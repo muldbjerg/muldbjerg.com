b1af436b2fd1fe8013e089c3675a32adc6073d70
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2023-03-21 22:25:12 +0100

    Removed old site

--- src/app/pages/blog/blog.config.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { InjectionToken } from '@angular/core';
-
-export const POSTS = new InjectionToken<Routes>('devblog.posts');
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  contentful: {
-    space: 'wnhx8rt8bx4x',
-    accessToken: 'VcwPYiKewYvDWnbOqLJS_48YVy_hH9yKq8odPN9nzCM',
-  },
-};
