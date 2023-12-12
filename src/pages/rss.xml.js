d51d72b28aa7d8ba8127994e9a56d3f65c4dff8d
Steffen Muldbjerg <steffenmuldbjerg@gmail.com>
2023-12-12 21:28:07 +0100

    Update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
-import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
+export async function GET(context) {
 	const posts = await getCollection('blog');
-	return rss({
+	
+	var rss = {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
@@ -12,5 +12,14 @@ export async function get(context) {
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
-	});
+	}
+
+	return new Response(
+		JSON.stringify(rss), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json"
+		}
+		}
+	);
 }
