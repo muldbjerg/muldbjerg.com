4c26d71fc2ae25ef5de5456b99d52d0716173929
Steffen Østerby Muldbjerg <steffenmuldbjerg@gmail.com>
2025-03-10 20:44:36 +0100

    Update

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="@astrojs/image/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_GOOGLE_SHEETS_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
